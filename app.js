fdd17e8ddba5ba298684f714dbc8c22ff5159a9d
Di Wu <95495325+dw2761@users.noreply.github.com>
2022-05-26 02:25:24 -0400

    Update pagination

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const favicon = require('serve-favicon');
 var path = require('path');
+const mongoosePaginate = require('mongoose-paginate-v2');
 
 
 
@@ -19,24 +20,32 @@ app.use(favicon(__dirname + '/public/favicon.ico'));
 mongoose.connect("mongodb+srv://admin:test123@cluster0.8d829.mongodb.net/inventoryDB", {useNewUrlParser: true});
 
 
-const itemsSchema = {
+const itemsSchema = new mongoose.Schema({
   name: String,
   category: String,
   quantity: Number,
   description: String,
   status: String
-};
+});
 
-
+itemsSchema.plugin(mongoosePaginate);
 
 const Item = mongoose.model("Item",itemsSchema);
 
 
-
-
 app.get("/", function(req, res){
-  Item.find({}, function(err, foundItems){
-    res.render("index", {newItems: foundItems});
+  var pagenum = req.query.pagenum ? req.query.pagenum : 1;
+  const options = {
+  page: pagenum,
+  limit: 5,
+  collation: {
+    locale: 'en',
+  },
+};
+
+  Item.paginate({}, options, function (err, result) {
+    const foundItems = result.docs;
+    res.render("index", {newItems: foundItems, result: result});
   });
 
 });
@@ -96,7 +105,7 @@ app.post("/delete", function(req, res){
 });
 
 
-
 app.listen(3000, function(){
   console.log("Server started on port 3000.");
 });
+
